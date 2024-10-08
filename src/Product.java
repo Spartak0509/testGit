aa20e0f09dd0e403ae9a0ac3a7dd5519ee8ce346
SpartakMS <Spartak.mukaev@mail.ru>
2024-10-08 16:02:27 +0300

    реализован конструктор product

--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,9 @@
 public class Product {
     String name;
     int calories;
+
+    public Product(String name, int calories) {
+        this.name = name;
+        this.calories = calories;
+    }
 }
